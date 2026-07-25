2b4bf62aa15687e96c9691708710dadb5d46ba56
sakibulislam75 <sakibulislam1623@gmail.com>
2026-07-25 10:29:41 +0600

    chore: setup basic express server with cors and json middleware

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("welcome to our simple crud server");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
