f0b9778dce8502fe5dac0116f693b3e534442818
sakibulislam75 <sakibulislam1623@gmail.com>
2026-07-26 08:19:57 +0600

    feat: setup express server and connect mongodb with users api

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(
+  "mongodb+srv://simple_crud_server:VTvM0OzMSCif0r9J@cluster0.mvh6hdg.mongodb.net/?appName=Cluster0",
+); // MongoClient instance তৈরি করছি
+
+const run = async () => {
+  try {
+    await client.connect(); // Cluster এ connect করার চেষ্টা
+    const db = client.db("simpleCrud");
+    const userCollection = db.collection("users");
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    console.log("You successfully connected to MongoDB!"); // সফল হলে console এ message দেখাবে
+  } catch (err) {
+    console.dir(err); // Error হলে পুরো object tree-style এ দেখাবে
+  }
+};
+
+run(); // Function call করছি যাতে connection establish হয়
 
 app.get("/", (req, res) => {
   res.send("welcome to our simple crud server");
